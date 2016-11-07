5168131e6edeec92b62c8b6de62ee38e1e684515
Nemanja Bocokic <nemanja.bocokic@gmail.com>
2016-11-07 16:34:36 +0100

    Revert "Update Update-Nsclient.ps1"
    This reverts commit bbbf24da4ee247c60433b41e4e8ba66f23a08f92.

--- Monitoring/Update-Nsclient.ps1
+++ Monitoring/Update-Nsclient.ps1
@@ -1,36 +1,36 @@
 # Wenn das nicht geht...
-Get-content ‚Äú.\hosts.txt‚Äù | foreach {
+Get-content ì.\hosts.txtî | foreach {
             New-PSSession -Computername $_
             Write-Host("NSCP gestoppt")
             If (test-path "\\$_\c$\Program Files\NSClient++\nsclient.ini") {
                         Remove-Item "\\$_\c$\Program Files\NSClient++\nsclient.ini"
-                        Write-Host("Ini gel√∂scht")
+                        Write-Host("Ini gelˆscht")
             }
-            Copy-Item "PATH_TO_WORKING_FILE\nsclient.ini" "\\$_\c$\Program Files\NSClient++\nsclient.ini"
+            Copy-Item "\\HLNT02\c$\Users\Administrator.HOEGGER\Desktop\nsclient.ini" "\\$_\c$\Program Files\NSClient++\nsclient.ini"
             Write-Host("Ini kopiert")
             Restart-Service nscp -force
             Write-Host("NSCP gestartet")
             Exit-PSSession
 }
 
-#... das hier ausf√ºhren...
-Get-content ‚Äú.\hosts.txt‚Äù | foreach {
+#... das hier ausf¸hren...
+Get-content ì.\hosts.txtî | foreach {
             If (test-path "\\$_\c$\Program Files\NSClient++\nsclient.ini") {
                         Remove-Item "\\$_\c$\Program Files\NSClient++\nsclient.ini"
-                        Write-Host("Ini gel√∂scht")
+                        Write-Host("Ini gelˆscht")
             }
-            Copy-Item "PATH_TO_WORKING_FILE\nsclient.ini" "\\$_\c$\Program Files\NSClient++\nsclient.ini"
+            Copy-Item "\\HLNT02\c$\Users\Administrator.HOEGGER\Desktop\nsclient.ini" "\\$_\c$\Program Files\NSClient++\nsclient.ini"
             Get-Service -Name nscp -ComputerName $_ | Restart-Service
 } 
 
 #... oder am besten das hier
-Get-content ‚Äú.\hosts.txt‚Äù | foreach {
+Get-content ì.\hosts.txtî | foreach {
     Restart-Service -InputObject $(Get-Service -Computer $_ -Name nscp);
 	If (test-path "\\$_\c$\Program Files\NSClient++\nsclient.ini") {
 		Remove-Item "\\$_\c$\Program Files\NSClient++\nsclient.ini"
-		Write-Host("Ini gel√∂scht")
+		Write-Host("Ini gelˆscht")
 	}
-	Copy-Item "PATH_TO_WORKING_FILE\nsclient.ini" "\\$_\c$\Program Files\NSClient++\nsclient.ini"
+	Copy-Item "\\SRVFM002\c$\Users\administrator.SCHNIDER-AG\Desktop\nsclient.ini" "\\$_\c$\Program Files\NSClient++\nsclient.ini"
 
 	Write-Host("NSCP neugstartet auf $_")
 }
